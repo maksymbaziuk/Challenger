7d76fd9b1f33544d9e8877abda16f02224c79214
Maksym Baziuk <maksym_baziuk@epam.com>
2016-01-04 19:39:56 +0200

    modifications into SH script

--- startServer.sh
+++ startServer.sh
@@ -1,2 +1,2 @@
 cd /home/repo/target/com.challenger-0.0.5-SNAPSHOT/WEB-INF/
-java -classpath "lib/*;classes/." com.challenger.ChallengerRunner
+java -classpath 'lib/*:classes/.' com.challenger.ChallengerRunner
